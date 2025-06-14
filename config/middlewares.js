532e47372258ea63b9b2880b745fae7652cd38bc
MrRozco <bryanorozco1997@hotmail.com>
2025-06-14 12:59:28 -0400

    added CORS

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,15 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://rozco-strapi-frontend.vercel.app/', 'http://localhost:3000'], // Add your Vercel domain and local dev URL
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      credentials: true, // Optional, only if your app requires cookies or sessions
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
